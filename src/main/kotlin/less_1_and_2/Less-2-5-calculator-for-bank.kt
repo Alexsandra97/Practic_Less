8d82d593a5b92890a74a4d8d7e2374630763c941
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-02 01:50:45 +1000

    PL-2-5-add code about calculator for bank

---
+++ src/main/kotlin/less_1_and_2/Less-2-5-calculator-for-bank.kt
@@ -0,0 +1,15 @@
+package less_1_and_2
+
+import kotlin.math.pow
+
+fun main() {
+
+    val depositPeriod = 20.0
+    val sumForTheDeposit = 70000
+    val interestRate = 16.7
+
+    val depositAmount = sumForTheDeposit * (1 + interestRate / 100).pow(depositPeriod)
+
+    println(String.format("%.3f", depositAmount))
+
+}
