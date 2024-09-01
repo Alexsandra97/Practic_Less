496bc5b36a47ae4042440ebfdfaa927183dec4b5
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-02 01:16:37 +1000

    PL-1-5-add code

---
+++ src/main/kotlin/less_1_and_2/Less-1-5-star.kt
@@ -0,0 +1,15 @@
+package less_1_and_2
+
+fun main() {
+
+    val TIME = 60
+
+    val seconds: Int = 6480
+
+    val minute: Int = (seconds % (TIME * TIME)) / TIME
+    val hour: Int = seconds / (TIME * TIME)
+    val secSecond: Int = (seconds % (TIME * TIME)) % TIME
+
+
+    println(String.format("%02d:%02d:%02d", hour, minute, secSecond))
+}
