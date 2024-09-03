5353a9b942dc33b34db99ef04fef5357dc393432
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-03 23:53:11 +1000

    PL-1-5- push ctrl + alt + L

--- src/main/kotlin/less_1_and_2/Less-1-5-star.kt
+++ src/main/kotlin/less_1_and_2/Less-1-5-star.kt
@@ -1,6 +1,6 @@
 package less_1_and_2
 
-const  val TIME = 60
+const val TIME = 60
 
 fun main() {
 
@@ -9,7 +9,6 @@ fun main() {
     val minute: Int = (seconds % (TIME * TIME)) / TIME
     val hour: Int = seconds / (TIME * TIME)
     val secSecond: Int = (seconds % (TIME * TIME)) % TIME
-
 
     println(String.format("%02d:%02d:%02d", hour, minute, secSecond))
 }
