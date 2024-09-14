30fd4b03717ddeff689a15c4babc4f2c6fd5e747
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-14 14:45:40 +1000

    KS-4-2 add code

---
+++ src/main/kotlin/KS-4-2-cargo-category-.kt
@@ -0,0 +1,15 @@
+const val MIN_WEIGHT_AVERAGE = 35
+const val MAX_WEIGHT_AVERAGE = 100
+const val MAX_VOLUME_AVERAGE = 100
+
+fun main() {
+
+    val weighrCargo1 = 20
+    val volumeCargo1 = 80
+    val weighrCargo2 = 50
+    val volumeCargo2 = 100
+
+    println("Груз с весом $weighrCargo1 кг и объемом $volumeCargo1 л Соответствует категории 'Averege': ${(MIN_WEIGHT_AVERAGE < weighrCargo1 && MAX_WEIGHT_AVERAGE >= weighrCargo1) && MAX_VOLUME_AVERAGE > volumeCargo1}")
+    println("Груз с весом $weighrCargo2 кг и объемом $volumeCargo2 л Соответствует категории 'Averege': ${(MIN_WEIGHT_AVERAGE < weighrCargo2 && MAX_WEIGHT_AVERAGE >= weighrCargo2) && MAX_VOLUME_AVERAGE > volumeCargo2}")
+
+}
