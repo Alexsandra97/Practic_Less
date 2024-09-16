bc81084d36e5e72e6b1a7c6074b959164cdaa31a
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-16 18:19:15 +1000

    KS-5-1 replacing const

--- src/main/kotlin/KS-5-1-authorization.kt
+++ src/main/kotlin/KS-5-1-authorization.kt
@@ -1,8 +1,10 @@
-const val TRUE_RESULT = 92
+const val FIRST_VARIABLE = 34
+const val SECOND_VARIABLE = 58
+const val TRUE_RESULT = FIRST_VARIABLE + SECOND_VARIABLE
 
 fun main() {
 
-    println("Для авторизации необходимо решить математический пример:\n 34 + 58 =")
+    println("Для авторизации необходимо решить математический пример:\n $FIRST_VARIABLE + $SECOND_VARIABLE =")
     var result: Int = readln().toInt()
 
     if (TRUE_RESULT == result)
