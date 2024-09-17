0a9a86bf6db5b900647b8d89ed3e08c0ca634680
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-17 12:59:06 +1000

    KS-5-1 del const

--- src/main/kotlin/KS-5-1-authorization.kt
+++ src/main/kotlin/KS-5-1-authorization.kt
@@ -1,13 +1,13 @@
-const val FIRST_VARIABLE = 34
-const val SECOND_VARIABLE = 58
-const val TRUE_RESULT = FIRST_VARIABLE + SECOND_VARIABLE
-
 fun main() {
 
-    println("Для авторизации необходимо решить математический пример:\n $FIRST_VARIABLE + $SECOND_VARIABLE =")
+    val firstVariable = 34
+    val secondVariable = 58
+    val trueResult = firstVariable + secondVariable
+
+    println("Для авторизации необходимо решить математический пример:\n $firstVariable + $secondVariable =")
     var result: Int = readln().toInt()
 
-    if (TRUE_RESULT == result)
+    if (trueResult == result)
         println("Добро пожаловать!")
     else
         println("Доступ запрещен.")
