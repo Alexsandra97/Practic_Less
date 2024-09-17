c721465671584cf0f68f524d63ddb32110166c67
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-17 13:06:31 +1000

    KS-5-3 change condition

--- src/main/kotlin/KS-5-3-lottery.kt
+++ src/main/kotlin/KS-5-3-lottery.kt
@@ -9,17 +9,17 @@ fun main() {
     println("Введите второе число от 0 до 42")
     var secondNumber: Int = readln().toInt()
 
-    if (firstNumber == firstWinningNumber){
-        if (secondNumber == secondWinningNumber)
-            println("Поздравляем! Вы выиграли главный приз!")
-        else
-            println("Вы выиграли утешительный приз!")
-    }else if (firstNumber == secondWinningNumber) {
-        if (secondNumber == firstWinningNumber)
-            println("Поздравляем! Вы выиграли главный приз!")
-        else
-            println("Вы выиграли утешительный приз!")
-    }else
+    if ((firstNumber == firstWinningNumber && secondNumber == secondWinningNumber) ||
+        (firstNumber == secondWinningNumber && secondNumber == firstWinningNumber)
+    )
+        println("Поздравляем! Вы выиграли главный приз!")
+    else if ((firstNumber == firstWinningNumber) ||
+        (secondNumber == secondWinningNumber) ||
+        (firstNumber == secondWinningNumber) ||
+        (secondNumber == firstWinningNumber)
+    )
+        println("Вы выиграли утешительный приз!")
+    else
         println("Неудача!")
 
     println("Для победы нужны числа $firstWinningNumber и $secondWinningNumber")
