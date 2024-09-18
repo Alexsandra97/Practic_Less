d762b0b4c806082b2c3a7f6d33fffc4cd4f7a038
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-18 18:51:28 +1000

    KS-7-3 correct code

--- src/main/kotlin/KS-7-3-even.kt
+++ src/main/kotlin/KS-7-3-even.kt
@@ -4,10 +4,9 @@ fun main() {
     var evenMax: Int = readln().toInt()
     var evenPrint = 0
 
-    for (i in 0..evenMax) {
-        if ((i % 2) == 0)
-            println(evenPrint)
-        evenPrint++
+    for (i in 0..evenMax step 2) {
+        println(evenPrint)
+        evenPrint += 2
     }
 
 }
