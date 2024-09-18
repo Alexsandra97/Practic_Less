1c1897eec28f960ca5471a4103eb3f645e2a61fb
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-18 17:16:46 +1000

    KS-8-4 correct code

--- src/main/kotlin/KS-8-4-ingredient replacement.kt
+++ src/main/kotlin/KS-8-4-ingredient replacement.kt
@@ -6,13 +6,13 @@ fun main() {
     println("Какой ингредиент хотите заменить?")
     var replacementIngredient = readln()
 
-    if (ingredientsDish.indexOf(replacementIngredient) < 0) {
+    if (ingredientsDish.indexOf(replacementIngredient) >= 0) {
+        println("Какой ингридиент хотите добавить?")
+        val newIngredient = readln().toString()
+        ingredientsDish[ingredientsDish.indexOf(replacementIngredient)] = newIngredient
+    } else {
         println("Такого ингредиента в рецепте нет")
         return
-    } else {
-        println("Какой ингридиент хотите добавить?")
-        var newIngredient = readln().toString()
-        ingredientsDish[ingredientsDish.indexOf(replacementIngredient)] = newIngredient
     }
 
     println("Готово! Вы сохранили следующий список: ${ingredientsDish.contentToString()} ")
