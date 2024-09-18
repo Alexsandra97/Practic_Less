b63504e1a442dad6314576dfcb61ff4d8be23ff2
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-18 17:10:54 +1000

    KS-8-5 correct code

--- src/main/kotlin/KS-8-5-arr-ingredients.kt
+++ src/main/kotlin/KS-8-5-arr-ingredients.kt
@@ -3,7 +3,7 @@ fun main() {
     println("Сколько будет элементов?")
     val sumIngredients: Int = readln().toInt()
 
-    val ingredients = Array(sumIngredients, {readln()})
+    val ingredients = Array(sumIngredients ) {readln()}
     println(ingredients.contentToString())
 
 }
