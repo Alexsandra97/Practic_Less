4978b4d831c78b0cfa67e4f5af8492ad66066984
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-25 17:10:16 +1000

    KS-11-1 add code

---
+++ src/main/kotlin/KS-11-1-user data.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    val user1: User = User(
+        id = 1,
+        login = "login1",
+        password = "password1",
+        email = "mail1@mail.ru"
+    )
+
+    val user2: User = User(
+        id = 2,
+        login = "login2",
+        password = "password2",
+        email = "mail2@mail.ru"
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.email)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.email)
+
+}
