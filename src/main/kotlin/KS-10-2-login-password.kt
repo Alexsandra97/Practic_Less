0f3d967ff11371138465b99d26c5e17e9888b8ad
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-25 16:26:04 +1000

    KS-10-2 correct code

--- src/main/kotlin/KS-10-2-login-password.kt
+++ src/main/kotlin/KS-10-2-login-password.kt
@@ -1,17 +1,24 @@
-fun checkLength() {
+const val MAX_LENGTH = 4
 
-        var onCheck = readln()
-        if (onCheck.length < 4) println("Логин или пароль недостаточно длинные")
+fun checkLength(onCheck: String): String {
+
+    var result = ""
+        if (onCheck.length < MAX_LENGTH)
+            result = "Логин или пароль недостаточно длинные"
+            return result
 }
 
 fun main() {
 
     println("Придумайте логин:")
-    val login = checkLength()
+    val login = checkLength(readln())
+    println(login)
 
     println("Введите пароль")
-    val password = checkLength()
+    val password = checkLength(readln())
+    println(password)
 
+    if (login == "" && password == "")
     println("Добро пожаловать!")
 
 }
