3417de7fc1685ef61a67bd376a464de5c51f1791
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-25 16:13:46 +1000

    KS-9-5 correct code

--- src/main/kotlin/KS-9-5-five-ingredientd-new.kt
+++ src/main/kotlin/KS-9-5-five-ingredientd-new.kt
@@ -2,18 +2,14 @@ const val LENGTH_LIST = 5
 
 fun main() {
     var ingredients: MutableSet<String> = mutableSetOf()
-    var firstElement = ""
 
     do {
         ingredients.add(readln())
     } while (ingredients.size != LENGTH_LIST)
 
     val sortIngredientList: MutableList<String> = ingredients.sorted() as MutableList<String>
-    for (i in sortIngredientList) {
-        if (sortIngredientList.indexOf(i) == 0) {
-            firstElement = i.replaceFirstChar { it.uppercase() }
-        }
-    }
-    sortIngredientList[0] = firstElement
-    println(sortIngredientList.joinToString())
+    println(
+        (sortIngredientList.joinToString(separator = ", ", postfix = "."))
+            .replaceFirstChar { it.uppercase() }
+    )
 }
