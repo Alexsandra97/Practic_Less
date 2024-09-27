31efb00c06d86ff4ad4bbe499723d20b1307665f
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-27 18:17:37 +1000

    KS-11-2 add class and correct

--- src/main/kotlin/KS-11-2-new-user-data.kt
+++ src/main/kotlin/KS-11-2-new-user-data.kt
@@ -7,6 +7,34 @@ fun main() {
     )
 
     user1.addBio()
-    user1.correctPassword()
+    user1.changePassword()
     user1.outputInformation()
+}
+
+class User2(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val email: String,
+    var bio: String = "",
+) {
+    fun outputInformation() {
+        println("id: $id\nлогин: $login\nпароль: $password\nпочта: $email\nbio: $bio")
+    }
+
+    fun addBio(): String {
+        println("Добавьте bio")
+        bio = readln()
+        return bio
+    }
+
+    fun changePassword() {
+        println("Для смены пароля введите старый пароль:")
+        if (readln() == password) {
+            println("Пароль верный. Введите новый пароль")
+            password = readln()
+            println("Пароль изменен.")
+        }
+
+    }
 }
