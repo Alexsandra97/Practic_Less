1546ef4e21dd5a1c424a7f6bd2f996a943db28d5
Alexsandra97 <thefirstone010101@gmail.com>
2024-09-30 23:55:47 +1000

    KS-11-4 correct  code

--- src/main/kotlin/KS-11-4-data-storage.kt
+++ src/main/kotlin/KS-11-4-data-storage.kt
@@ -1,14 +1,21 @@
 class RecipeCategory(
-    val dishCategory: List<Recipe>
+    val id: Int,
+    val dishCategory: List<Recipe>,
+    val nameDishCategory: String,
+    val descriptionDishCategory: String,
+    val imageUrl: String
 )
 
 class Recipe(
     val ingredient: List<Ingredient>,
     val recipe: String,
-    val favourites: Boolean,
+    val hasFavourites: Boolean,
+    val imageUrl: String
 )
 
 class Ingredient(
     val ingredient: MutableList<String>,
-    val sumPortion: Int
+    val sumPortion: Int,
+    val measureUnit: String,
+    val descriptionIngredient: String,
 )
