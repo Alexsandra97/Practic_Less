ff8cb85b67e9d01ce49fcc076bd1ca14c804cf68
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-01 23:57:08 +1000

    KS-12-4 correct code

--- src/main/kotlin/KS-12-4-weather.kt
+++ src/main/kotlin/KS-12-4-weather.kt
@@ -1,25 +1,21 @@
-const val KELVIN_TEMPERATUR = 273.15f
 
 class NewWeatherDataFour(_dayTemperature: Int, _nightTemperature: Int, _presencePrecipitation: Boolean) {
     var dayTemperature = _dayTemperature
     var nightTemperature = _nightTemperature
     var presencePrecipitation = _presencePrecipitation
+
+    init {
+        println(
+            "dayTemperature = ${dayTemperature}\n" +
+                    "nightTemperature = ${nightTemperature}\n" +
+                    "presencePrecipitation = ${presencePrecipitation}"
+        )
+    }
 }
 
 fun main() {
 
-    val firstObject = NewWeatherDataFour((23 + KELVIN_TEMPERATUR).toInt(), (16 + KELVIN_TEMPERATUR).toInt(), false)
-    println(
-        "dayTemperature = ${firstObject.dayTemperature}\n" +
-                "nightTemperature = ${firstObject.nightTemperature}\n" +
-                "presencePrecipitation = ${firstObject.presencePrecipitation}"
-    )
-
-    val secondObject = NewWeatherDataFour((21 + KELVIN_TEMPERATUR).toInt(), (12 + KELVIN_TEMPERATUR).toInt(), true)
-    println(
-        "dayTemperature = ${secondObject.dayTemperature}\n" +
-                "nightTemperature = ${secondObject.nightTemperature}\n" +
-                "presencePrecipitation = ${secondObject.presencePrecipitation}"
-    )
+    NewWeatherDataFour(23, 16,  false)
+    NewWeatherDataFour(21, 12, true)
 
 }
