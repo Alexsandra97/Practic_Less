2584aa7ee43ce2ca3f071e85cb127a5f38dbb7de
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-04 01:53:53 +1000

    KS-14-4 add code

---
+++ src/main/kotlin/KS-14-4-game-planet.kt
@@ -0,0 +1,19 @@
+open class CelestialBodies(val nameCelestialBodies: String) {
+    fun printName() {
+        println(nameCelestialBodies)
+    }
+}
+
+class Planet(nameCelestialBodies: String, val listSatellite: List<Satellite>): CelestialBodies(nameCelestialBodies)
+
+class Satellite(nameCelestialBodies: String): CelestialBodies(nameCelestialBodies)
+
+fun main() {
+
+    val planet = Planet("Астерион", listOf(Satellite("Астор"), Satellite("Гастор")))
+    println("Название планеты:")
+    planet.printName()
+    println("Название ее спутников:")
+    planet.listSatellite.forEach{it.printName()}
+
+}
