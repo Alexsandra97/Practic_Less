4b8cebcbfc82304c7dda67641d652e9f6f06c693
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-05 18:29:51 +1000

    KS-14-4 correct code

--- src/main/kotlin/KS-14-4-game-planet.kt
+++ src/main/kotlin/KS-14-4-game-planet.kt
@@ -1,16 +1,16 @@
-open class CelestialBodies(val nameCelestialBodies: String) {
+open class CelestialBodies(val nameCelestialBodies: String, hasAtmosphere: Boolean, isSuitableForDisembarkation: Boolean) {
     fun printName() {
         println(nameCelestialBodies)
     }
 }
 
-class Planet(nameCelestialBodies: String, val listSatellite: List<Satellite>) : CelestialBodies(nameCelestialBodies)
+class Planet(nameCelestialBodies: String, hasAtmosphere: Boolean, isSuitableForDisembarkation: Boolean, val listSatellite: List<Satellite>) : CelestialBodies(nameCelestialBodies, hasAtmosphere, isSuitableForDisembarkation)
 
-class Satellite(nameCelestialBodies: String) : CelestialBodies(nameCelestialBodies)
+class Satellite(nameCelestialBodies: String, hasAtmosphere: Boolean, isSuitableForDisembarkation: Boolean) : CelestialBodies(nameCelestialBodies, hasAtmosphere, isSuitableForDisembarkation)
 
 fun main() {
 
-    val planet = Planet("Астерион", listOf(Satellite("Астор"), Satellite("Гастор")))
+    val planet = Planet("Астерион", true, true, listOf(Satellite("Астор", false, false), Satellite("Гастор", true, false)))
     println("Название планеты:")
     planet.printName()
     println("Название ее спутников:")
