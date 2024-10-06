efc9543d56736c25f089240b8a4c7d55f67fe639
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-06 19:00:08 +1000

    KS-15-2 correct code

--- src/main/kotlin/KS-15-2-weather conditions.kt
+++ src/main/kotlin/KS-15-2-weather conditions.kt
@@ -1,23 +1,23 @@
-abstract class WeatherStationStats{
-    abstract val data: Int
-    abstract fun sendMessage()
+abstract class WeatherServer{
+    abstract val WeatherStationStat: Int
+    abstract fun sendMessage(WeatherStationStat: Int)
 }
 
-class Temperature(override val data: Int): WeatherStationStats() {
-    override fun sendMessage() {
-        println("Температура: $data")
+class Temperature(override val WeatherStationStat: Int): WeatherServer() {
+    override fun sendMessage(WeatherStationStat: Int) {
+        println("Температура: $WeatherStationStat")
     }
 }
 
-class PrecipitationAmount(override val data: Int): WeatherStationStats() {
-    override fun sendMessage() {
-        println("Количество осадков: $data")
+class PrecipitationAmount(override val WeatherStationStat: Int): WeatherServer() {
+    override fun sendMessage(WeatherStationStat: Int) {
+        println("Количество осадков: $WeatherStationStat")
     }
 }
 
 fun main() {
     val object1 = Temperature(13)
-    object1.sendMessage()
+    object1.sendMessage(13)
     val object2 = PrecipitationAmount(7)
-    object2.sendMessage()
+    object2.sendMessage(7)
 }
