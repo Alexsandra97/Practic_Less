e9f0dc9e3026e4ab6fd9ddd8ea16257e9f1be45d
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-12 02:12:13 +1000

    KS-11-5-correct code

--- src/main/kotlin/KS-11-5-Forum.kt
+++ src/main/kotlin/KS-11-5-Forum.kt
@@ -1,50 +1,57 @@
 
-open class Forum(
-    var id: Int = 0,
-    var text: String,
-    var listUser: MutableList<String> = mutableListOf()
-) {
+open class Forum {
+    open var id: Int = 0
+    open var text: String = ""
+    open var listUser: MutableList<Forum> = mutableListOf()
 
-    fun createNewUser(text: String): String {
-        val memberForum = text.toString()
+    fun createNewUser(text: String): MemberForum {
+        id = listUser.count()
+        val memberForum = MemberForum(id, text)
         listUser.add(memberForum)
-        ++id
         return memberForum
     }
 
-    fun createNewMessae(userId: Int): String {
+
+
+
+    fun createNewMessae(userId: Int) {
         val message = readln()
         id = userId
        // if ((message.find { it.userId == userId }) != null) {
         //    text = readln()
          //   id = userId
        // }
-
-        if (listUser.find { it })
+       // if (listUser.find { it })
 
     }
 }
 
 
 fun main() {
-    val a1 = Forum("")
+    val object1 = Forum()
+    object1.createNewUser("юзер1")
+    object1.listUser.forEach { it -> println(it.id)
+        println(it.text)}
+
+    val object2 = Forum()
+    object2.createNewUser("юзер2")
+    object2.listUser.forEach { it -> println(it.id)
+    println(it.text)}
 }
 
-class MemberForum(override val id: Int, override val text: String: String):Forum(id, text) {
-
-    override var id: Int
+class MemberForum(val userId: Int, val userName: String) : Forum() {
+    override var id: Int = 0
         get() = userId
     override var text: String = ""
         get() = userName
-
-
 }
 
 class ForumMessage(
     val authorId: Int,
     val message: String,
-) : Forum {
-    override fun create() {
-
-    }
+) : Forum() {
+    override var id: Int = 0
+        get() = authorId
+    override var text: String = ""
+        get() = message
 }
