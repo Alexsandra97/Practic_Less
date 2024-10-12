4bcd65b0f5989405b17afd81dffbde659b0ba111
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-12 19:29:54 +1000

    KS-11-5-correct code

--- src/main/kotlin/KS-11-5-Forum.kt
+++ src/main/kotlin/KS-11-5-Forum.kt
@@ -1,42 +1,50 @@
-
-open class Forum {
+open class Forum() {
     open var id: Int = 0
     open var text: String = ""
     open var listUser: MutableList<Forum> = mutableListOf()
 
+
     fun createNewUser(text: String): MemberForum {
-        id = listUser.count()
+        id = listUser.size
         val memberForum = MemberForum(id, text)
         listUser.add(memberForum)
         return memberForum
     }
 
+    fun createNewMessae(id: Int) {
 
+        if (listUser.find { (it.id == id) } != null) {
+            println("пользователь найден. введите сообщение")
+            text = readln()
+            val forumMessage = ForumMessage(id, text)
+            listUser.add(forumMessage)
+        }
+    }
 
-
-    fun createNewMessae(userId: Int) {
-        val message = readln()
-        id = userId
-       // if ((message.find { it.userId == userId }) != null) {
-        //    text = readln()
-         //   id = userId
-       // }
-       // if (listUser.find { it })
-
+    fun printThread() {
+        for (i in 0..1) {
+            listUser.forEach { it ->
+                if (it.javaClass.name == "MemberForum" && it.id == i) {
+                    print("${ it.text }: ")}
+                    if (it.javaClass.name == "ForumMessage" && it.id == i) {println(it.text) }
+            }
+        }
     }
 }
 
 
 fun main() {
-    val object1 = Forum()
-    object1.createNewUser("юзер1")
-    object1.listUser.forEach { it -> println(it.id)
-        println(it.text)}
+    val example = Forum()
+    example.createNewUser("user1")
+    example.createNewUser("user2")
+    example.listUser.forEach { it -> println("${it.id} ${it.text} ${it.javaClass}") }
 
-    val object2 = Forum()
-    object2.createNewUser("юзер2")
-    object2.listUser.forEach { it -> println(it.id)
-    println(it.text)}
+    example.createNewMessae(1)
+    example.createNewMessae(0)
+    example.listUser.forEach { it -> println("${it.id} ${it.text} ${it.javaClass}") }
+
+    example.printThread()
+
 }
 
 class MemberForum(val userId: Int, val userName: String) : Forum() {
