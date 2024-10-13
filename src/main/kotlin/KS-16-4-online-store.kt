c8d5e81d408ec2ffd5630549a3707590c15221a8
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-13 21:25:10 +1000

    KS-16-4-correct code

--- src/main/kotlin/KS-16-4-online-store.kt
+++ src/main/kotlin/KS-16-4-online-store.kt
@@ -1,22 +1,21 @@
-class OnlineOrdering(private val numberOrdering: Int = 0) {
-    private var readyStatus: String = ""
+class OnlineOrdering(private val numberOrdering: Int) {
+    private var readyStatus: String = "в работе"
+    fun getStatus() = readyStatus
 
     private fun changeStatus(newStatus: String) {
-        println("Для смены статуса отправте заявку менеджеру с новым статусом")
-        val newStatus = readln()
+        readyStatus = newStatus
     }
 
-    fun sendRequest(newStatus: String) {
-        readyStatus = newStatus
+    fun sendRequest() {
+        println("Для смены статуса отправте заявку менеджеру с новым статусом")
+        val newStatus = readln()
         changeStatus(newStatus)
     }
 }
 
-
-
 fun main() {
     val order = OnlineOrdering(4)
-    println(order)
-    order.sendRequest("В работе")
-    println()
+    println(order.getStatus())
+    order.sendRequest()
+    println(order.getStatus())
 }
