b396471b11dad19ec55c6b7d5fc41253a27e3c72
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-19 11:00:16 +1000

    KS-19-1-correct code

--- src/main/kotlin/KS-19-1-aquarium.kt
+++ src/main/kotlin/KS-19-1-aquarium.kt
@@ -1,29 +1,19 @@
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHT_FISH
+enum class Fish(val fish: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHT_FISH("Петушок")
 }
 
 fun main() {
 
-    val listFish = listOf(101, 102, 103, 104)
+    for (cardType in Fish.values()) {
+        println("${cardType.fish} можно добавить в аквариум")
+    }
 
-    fun setStatus(fish: Fish) {
-        when (fish) {
-            Fish.GUPPY -> println("В аквариум можно добавить гуппи")
-            Fish.ANGELFISH -> println("В аквариум можно добавить скалярию")
-            Fish.GOLDFISH -> println("В аквариум можно добавить золотую рыбку")
-            Fish.SIAMESE_FIGHT_FISH -> println("В аквариум можно добавить петушка")
-        }
-    }
-    for (i in listFish) {
-        when (i) {
-            101 -> setStatus(Fish.GUPPY)
-            102 -> setStatus(Fish.ANGELFISH)
-            103 -> setStatus(Fish.GOLDFISH)
-            104 -> setStatus(Fish.SIAMESE_FIGHT_FISH)
-        }
-        Thread.sleep(1000)
-    }
 }
+
+
+
+
+
