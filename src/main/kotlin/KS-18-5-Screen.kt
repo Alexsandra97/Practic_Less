8887ca62922369f7794098a39d119d9f6e7b31f6
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-21 23:09:58 +1000

    KS-18-5-correct code

--- src/main/kotlin/KS-18-5-Screen.kt
+++ src/main/kotlin/KS-18-5-Screen.kt
@@ -1,6 +1,6 @@
- class Screen {
+class Screen {
 
-fun drawCircle(x: Int, y: Int): String {
+    fun drawCircle(x: Int, y: Int): String {
         return "Нарисовали круг в координатах ($x, $y)"
     }
 
@@ -26,15 +26,12 @@ fun drawCircle(x: Int, y: Int): String {
 
 }
 
-class Circle() {
-   fun <T> drawCircle(x: T, y: T ) where T: Screen{
-    drawCircle(x, y)
-   }
-
-
-}
-
 fun main() {
-    val circle = Circle()
-    print(circle.drawCircle(2, 3))
+    val screen = Screen()
+    screen.drawCircle(1, 2)
+    screen.drawCircle(1.5f, 6.9f)
+    screen.drawSquare(2, 4)
+    screen.drawSquare(2.1f, 4.4f)
+    screen.drawPoint(4, 6)
+    screen.drawPoint(5.7f, 1.3f)
 }
