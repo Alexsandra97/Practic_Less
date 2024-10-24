8f0683078bb339e98cbff01ebf3700c4e909f2ae
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-24 21:09:45 +1000

    KS-20-3-Key

--- src/main/kotlin/KS-8-1-skript.kt
+++
@@ -1,8 +0,0 @@
-const val WEEK = 7
-
-fun main() {
-
-    val numberViews: Array<Int> = Array(WEEK) { (0..100).random() }
-    println("За неделю было ${numberViews.sum()} просмотра")
-
-}
--- src/main/kotlin/KS-9-1-list-ingredients.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-
-    val listIngredients: List<String> = listOf("помидор", "огурец", "масло","специи", "сыр")
-    println("В рецепте есть следующие ингредиенты: ")
-    listIngredients.forEach{ println(it) }
-
-}
