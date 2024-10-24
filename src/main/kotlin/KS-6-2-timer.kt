f2d9a64f5611891a827f80aea526fa440706ec86
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-24 21:19:35 +1000

    KS-20-2-correct

--- src/main/kotlin/KS-6-2-timer.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-
-    println("Сколько секунд нужно засечь?")
-    var markSecond: Long = readln().toLong()
-
-    Thread.sleep(1000 * markSecond)
-    println("Прошло $markSecond секунд")
-
-}
