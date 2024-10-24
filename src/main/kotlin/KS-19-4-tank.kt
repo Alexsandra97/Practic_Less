1352825be5659b1c6d899bda278699270b1cd700
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-25 00:07:45 +1000

    KS-19-4-correct

--- src/main/kotlin/KS-19-4-tank.kt
+++ src/main/kotlin/KS-19-4-tank.kt
@@ -1,33 +1,39 @@
 enum class Cartridges(val impactForce: Int) {
     BLUE(5),
     GREEN(10),
-    RED(20)
+    RED(20),
+    NO_CARTRIDGE(0)
 }
 
-class Tank(val cartridge: Cartridges) {
-    fun armCartridge() {
+class Tank() {
+    var cartridge = Cartridges.NO_CARTRIDGE
+        fun armCartridge(impactForce: Cartridges) {
+        cartridge = impactForce
         when (cartridge) {
             Cartridges.BLUE -> println("Оружие заряжено синим патроном.")
             Cartridges.GREEN -> println("Оружие заряжено зеленым патроном.")
             Cartridges.RED -> println("Оружие заряжено красным патроном.")
+            Cartridges.NO_CARTRIDGE -> println("Оружие не заряжено.")
         }
+
     }
     fun shoot() {
         when (cartridge) {
             Cartridges.BLUE -> println("Произошел выстрел. Урон от выстрела ${Cartridges.BLUE.impactForce}")
             Cartridges.GREEN -> println("Произошел выстрел. Урон от выстрела ${Cartridges.GREEN.impactForce}")
             Cartridges.RED -> println("Произошел выстрел. Урон от выстрела ${Cartridges.RED.impactForce}")
+            Cartridges.NO_CARTRIDGE -> println("Выстрел не произведен. Урон ${Cartridges.RED.impactForce}")
         }
     }
 }
 
 fun main() {
-    val tank1 = Tank(Cartridges.RED)
-    tank1.armCartridge()
+    val tank1 = Tank()
+    tank1.armCartridge(Cartridges.RED)
     tank1.shoot()
     println()
 
-    val tank2 = Tank(Cartridges.GREEN)
-    tank2.armCartridge()
+    val tank2 = Tank()
+    tank2.armCartridge(Cartridges.GREEN)
     tank2.shoot()
 }
