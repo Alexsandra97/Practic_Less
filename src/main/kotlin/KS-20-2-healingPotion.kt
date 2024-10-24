9978a3d949cf5677f8ea45ee03cf5a4e405dbd44
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-24 21:09:52 +1000

    KS-20-3-Key

--- src/main/kotlin/KS-20-2-healingPotion.kt
+++ src/main/kotlin/KS-20-2-healingPotion.kt
@@ -2,9 +2,8 @@ class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
 
 fun main() {
     val player = Player("Игрок", 4, 10)
-    val healingPotion: (Player) -> Int =
-        fun(player: Player) = player.maxHealth
-    player.currentHealth = healingPotion.invoke(player)
+    val healingPotion: (Player) -> Unit = { it: Player -> player.currentHealth = player.maxHealth }
+    healingPotion.invoke(player)
     println(player.currentHealth)
 }
 
