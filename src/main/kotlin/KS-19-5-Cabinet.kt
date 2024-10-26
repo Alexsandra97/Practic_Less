923fa95295c46ce8540eb80dea3f7f64eac87741
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-26 19:37:39 +1000

    KS-19-5-correct

--- src/main/kotlin/KS-19-5-Cabinet.kt
+++ src/main/kotlin/KS-19-5-Cabinet.kt
@@ -1,22 +1,26 @@
-enum class Gender() {
+enum class Gender {
     MALE,
-    FEMALE
+    FEMALE,
+    ERROR
 }
 
 data class Cabinet(val name: String, val gender: Gender)
 
 fun main() {
     val gender: (String) -> Gender
-    gender = { it: String -> Gender.valueOf(it) }
+    gender = { it: String -> if (it =="MALE" || it == "FEMALE") Gender.valueOf(it) else  Gender.ERROR }
 
     val listCards = mutableListOf<Cabinet>()
     var isFlag = true
+
     while (isFlag == true) {
         println("Для создания введите имя и пол(MALE/FEMALE)")
         listCards.add(Cabinet(readln(), gender(readln())))
+
         if (listCards.count() == 5) listCards.forEach { it -> println(it) }
         println("Хотите добавить еще одну карточку?(да/нет)")
         val answer = readln()
+
         if (answer != "да") isFlag = false
     }
 
