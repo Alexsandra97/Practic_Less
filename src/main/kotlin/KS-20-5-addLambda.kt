1cdebfa1aa54c27b44ac00446d57bc93b1813891
Alexsandra97 <thefirstone010101@gmail.com>
2024-10-26 23:36:01 +1000

    KS-20-5 correct

--- src/main/kotlin/KS-20-5-addLambda.kt
+++ src/main/kotlin/KS-20-5-addLambda.kt
@@ -1,20 +1,23 @@
-fun say(phrase: List<String>) {
-    println(phrase.random().toString())
-}
-
-fun setModifier(phrase: List<String>, inventing: (List<String>) -> List<String>): List<String> {
-    println("Модификатор включен")
-    return inventing(phrase)
-}
-
-
 fun main() {
-
-    var phrase = listOf<String>("Добрый день", "Привет", "Здравствуйте", "Hello", "Hi")
     val addLambda = { phrase: List<String> -> phrase.map { it -> it.reversed() } }
+    val robot = Robot()
 
-    say(phrase)
-    phrase = setModifier(phrase, inventing = addLambda)
-    say(phrase)
+    robot.say()
+    robot.phrase = robot.setModifier(robot.phrase, inventing = addLambda)
+    robot.say()
 
+}
+
+class Robot() {
+    var phrase = listOf<String>("Добрый день", "Привет", "Здравствуйте", "Hello", "Hi")
+
+    fun say() {
+        println(phrase.random().toString())
+    }
+
+    fun setModifier(phrase: List<String>, inventing: (List<String>) -> List<String>): List<String> {
+        val phrase: List<String> = phrase
+        println("Модификатор включен")
+        return inventing(phrase)
+    }
 }
